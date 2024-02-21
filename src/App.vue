35f81670284dc6b40f9c433e3c4d29b501180143
soetl <soetl@proton.me>
2024-02-21 14:35:05 +0200

    setup frontend skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,35 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
+import { mdiPencil, mdiPlus } from '@mdi/js';
 </script>
 
 <template>
-  <div class="container">
-    <h1>Welcome to Tauri!</h1>
+  <v-app>
+    <v-layout>
+      <v-app-bar>
+        <template v-slot:extension>
+          <v-app-bar-nav-icon />
+        </template>
 
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
+        <v-app-bar-title text="cryptoscan" />
 
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
+        <template v-slot:append>
+          <v-btn :icon="mdiPencil" />
+          <v-btn :icon="mdiPlus" />
+        </template>
+      </v-app-bar>
 
-    <p>
-      Recommended IDE setup:
-      <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-      +
-      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-      +
-      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank"
-        >Tauri</a
-      >
-      +
-      <a href="https://github.com/rust-lang/rust-analyzer" target="_blank"
-        >rust-analyzer</a
-      >
-    </p>
+      <v-navigation-drawer>
+        <v-list>
+          <v-list-item title="Force reload" />
+          <v-list-item title="Change API key" />
+          <v-list-item title="Clear all coins" />
+          <v-list-item title="Toggle theme" />
+        </v-list>
+      </v-navigation-drawer>
 
-    <Greet />
-  </div>
+      <v-main>
+        
+      </v-main>
+    </v-layout>
+  </v-app>
 </template>
-
-<style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from "vue";
-import "./styles.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
+createApp(App).use(vuetify).mount("#app");
