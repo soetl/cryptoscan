77cdcc7017873392f1e5f03003eccc8853add86e
soetl <soetl@proton.me>
2024-02-25 21:13:30 +0200

    CoinCard realization

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,62 @@
 <script setup>
+import { ref } from 'vue';
+import { invoke } from "@tauri-apps/api/core";
+
 import { mdiPencil, mdiPlus } from '@mdi/js';
+
+import CoinCard from './components/CoinCard.vue';
+
+const theme = ref('light');
+const navDrawer = ref(false);
+const editMode = ref(false);
+
+const coins = ref([]);
+coins.value.push({
+  id: 1,
+  name: "Bitcoin",
+  symbol: "BTC",
+  price: 0,
+  percent_change_24h: 0,
+});
+
+function removeCoin(id, symbol) {
+  coins.value = coins.value.filter((coin) => coin.id !== id);
+}
+
+function invokeGet() {
+  invoke("get_coins").then((result) => {
+    coins.value = result;
+  });
+}
 </script>
 
 <template>
-  <v-app>
+  <v-app :theme="theme">
     <v-layout>
       <v-app-bar>
         <template v-slot:prepend>
-          <v-app-bar-nav-icon />
+          <v-app-bar-nav-icon @click.stop="navDrawer = !navDrawer" />
         </template>
 
         <v-app-bar-title text="cryptoscan" />
 
         <template v-slot:append>
-          <v-btn :icon="mdiPencil" />
-          <v-btn :icon="mdiPlus" />
+          <v-btn :icon="mdiPencil" @click.stop="editMode = !editMode" />
+          <v-btn :icon="mdiPlus" @click.stop="invokeGet" />
         </template>
       </v-app-bar>
 
-      <v-navigation-drawer>
+      <v-navigation-drawer v-model="navDrawer">
         <v-list>
-          <v-list-item title="Force reload" />
+          <v-list-item title="Force update" />
           <v-list-item title="Change API key" />
           <v-list-item title="Clear all coins" />
-          <v-list-item title="Toggle theme" />
+          <v-list-item title="Toggle theme" @click.stop="theme = theme === 'light' ? 'dark' : 'light'" />
         </v-list>
       </v-navigation-drawer>
 
       <v-main>
-        
+        <coin-card v-for="coin in coins" :key="coin.id" :coin="coin" :editMode="editMode" @removeCoin="removeCoin" />
       </v-main>
     </v-layout>
   </v-app>
---
+++ src/components/CoinCard.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { ref, computed } from "vue";
+
+import { mdiDelete } from "@mdi/js";
+
+const props = defineProps({
+  coin: {
+    type: Object,
+    required: true,
+  },
+  editMode: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["removeCoin"]);
+
+const showDetails = computed(() => {
+  return !(props.coin.price == 0 && props.coin.percent_change_24h == 0);
+});
+const theme = ref("light");
+
+const percentColor = computed(() => {
+  return props.coin.percent_change_24h > 0 ? "green" : "red";
+});
+</script>
+
+<template>
+  <v-card :title="coin.symbol + ' - ' + coin.name">
+    <template v-slot:prepend>
+      <v-card>
+        <v-img
+          width="42"
+          :aspect-ratio="1 / 1"
+          :src="
+            'https://s2.coinmarketcap.com/static/img/coins/64x64/' +
+            coin.id +
+            '.png'
+          "
+          cover
+          :alt="coin.symbol"
+        />
+      </v-card>
+    </template>
+    <template v-slot:append>
+      <v-btn
+        icon
+        density="comfortable"
+        v-if="props.editMode"
+        @click="emit('removeCoin', coin.id, coin.symbol)"
+        variant="text"
+      >
+        <v-icon color="error" :icon="mdiDelete" />
+      </v-btn>
+    </template>
+    <v-card-text class="d-flex">
+      {{ coin.price }}
+      <v-spacer />
+      <span :style="{ color: percentColor }">{{ coin.percent_change_24h }}</span>
+    </v-card-text>
+  </v-card>
+</template>
